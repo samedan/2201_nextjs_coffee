42a8499e0b5bc79cf333f704be4d9a247d4e91ed
Daniel Popescu <same.dan@gmail.com>
2022-02-18 15:48:51 +0100

    Finished friday

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -7,12 +7,34 @@ const table = base("coffee-stores");
 
 console.log({ table });
 
-const createCoffeStore = (req, res) => {
+const createCoffeStore = async (req, res) => {
   console.log({ req });
+
   if (req.method === "POST") {
-    res.json({ message: "POST message" });
-  } else {
-    res.json({ message: "GET message" });
+    try {
+      // find a record
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id="0"`,
+        })
+        .firstPage();
+
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = findCoffeeStoreRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      } else {
+        // create a record
+        res.json({ message: "Create a record" });
+      }
+    } catch (error) {
+      console.error("Error finding store", error);
+      res.status(500);
+      res.json({ message: "Error finding store", error });
+    }
   }
 };
 
