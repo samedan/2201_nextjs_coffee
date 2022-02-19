1a3eb62386eac56a072fd1bc0b93f0a3d8c64664
Daniel Popescu <same.dan@gmail.com>
2022-02-19 11:32:14 +0100

    CreateCoffeePlace API works

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -12,14 +12,15 @@ const createCoffeStore = async (req, res) => {
 
   if (req.method === "POST") {
     try {
-      // find a record
+      // find a record and NORMALIZE
       const findCoffeeStoreRecords = await table
         .select({
-          filterByFormula: `id="0"`,
+          filterByFormula: `id="3"`,
         })
         .firstPage();
 
       if (findCoffeeStoreRecords.length !== 0) {
+        console.log("found store");
         const records = findCoffeeStoreRecords.map((record) => {
           return {
             ...record.fields,
@@ -28,7 +29,25 @@ const createCoffeStore = async (req, res) => {
         res.json(records);
       } else {
         // create a record
-        res.json({ message: "Create a record" });
+        const createRecords = await table.create([
+          {
+            fields: {
+              id: "1",
+              name: "My favourite coffee store",
+              address: "Conflans",
+              neighbourhood: "some y place",
+              voting: 200,
+              imgUrl: "http://myimage.com",
+            },
+          },
+        ]);
+
+        const records = createRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
       }
     } catch (error) {
       console.error("Error finding store", error);
